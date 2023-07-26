b335cbdc0a9f83842bffe98a170719b73e453e89
Pheobhe <l.defalco@mjus.gba.gob.ar>
2023-07-26 11:24:59 -0300

    error:Vue is not a constructor

--- practica.js
+++ practica.js
@@ -1,6 +1,37 @@
-new Vue({
-el: '#app',
-data: {
-    saludar: 'Hola desde la instancia app'
-}
+
+var app = new Vue({
+    
+    el: '#app',
+    data: {
+        saludar: 'Hola desde la instancia app',
+        numero: 10,
+        lista:['1','2','3'],
+        activo: true
+        }
 });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'Usted cargó esta página el ' + new Date().toLocaleString()
+    }
+  });
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  });
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hola Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  });
