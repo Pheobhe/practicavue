3e102b51ba5f2a5ef36e77c824bdea84abd99bcf
Pheobhe <l.defalco@mjus.gba.gob.ar>
2023-07-26 12:14:04 -0300

    error:Vue is not a constructor

--- practica.js
+++ practica.js
@@ -1,37 +1,50 @@
+const { createApp } = Vue 
 
-var app = new Vue({
+createApp({
+  data() {
+    return {
+      saludar: 'Hola mundo',
+      mensaje: 'otra cosa'
+    }
+  }
+}).mount('#app')
+
+
+
+
+// document.onload = new Vue({
     
-    el: '#app',
-    data: {
-        saludar: 'Hola desde la instancia app',
-        numero: 10,
-        lista:['1','2','3'],
-        activo: true
-        }
-});
+//     el: '#app',
+//     data: {
+//         saludar: 'Hola desde la instancia app',
+//         numero: 10,
+//         lista:['1','2','3'],
+//         activo: true
+//         }
+// });
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'Usted cargó esta página el ' + new Date().toLocaleString()
-    }
-  });
+// var app2 = new Vue({
+//     el: '#app-2',
+//     data: {
+//       message: 'Usted cargó esta página el ' + new Date().toLocaleString()
+//     }
+//   });
 
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  });
+//   var app3 = new Vue({
+//     el: '#app-3',
+//     data: {
+//       seen: true
+//     }
+//   });
 
-  var app5 = new Vue({
-    el: '#app-5',
-    data: {
-      message: 'Hola Vue.js!'
-    },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split('').reverse().join('')
-      }
-    }
-  });
+//   var app5 = new Vue({
+//     el: '#app-5',
+//     data: {
+//       message: 'Hola Vue.js!'
+//     },
+//     methods: {
+//       reverseMessage: function () {
+//         this.message = this.message.split('').reverse().join('')
+//       }
+//     }
+//   });
