66a2c210379842347607fd61367c9080b20e600b
Lorena Defalco <l.defalco@mjus.gba.gob.ar>
2023-07-28 12:57:12 -0300

    ordenamiento

--- practica.js
+++ practica.js
@@ -7,46 +7,10 @@ createApp({
       mensaje: 'otra cosa',
       numero: 10,
       lista: ['1', '2', '3', '4', '5'],
-      activo: true
+      activo: false
     }
   }
 }).mount('#app')
 
-// var app5 = new Vue({
-//   el: '#app-5',
-//   data: {
-//     message: 'Hola Vue.js!'
-//   },
-//   methods: {
-//     reverseMessage: function () {
-//       this.message = this.message.split('').reverse().join('')
-//     }
-//   }
-// });
-
-// document.onload = new Vue({
-    
-//     el: '#app',
-//     data: {
-//         saludar: 'Hola desde la instancia app',
-//         numero: 10,
-//         lista:['1','2','3'],
-//         activo: true
-//         }
-// });
-
-// var app2 = new Vue({
-//     el: '#app-2',
-//     data: {
-//       message: 'Usted cargó esta página el ' + new Date().toLocaleString()
-//     }
-//   });
-
-//   var app3 = new Vue({
-//     el: '#app-3',
-//     data: {
-//       seen: true
-//     }
-//   });
 
  
